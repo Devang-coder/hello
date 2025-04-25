3f20d64110c5e5e784ae6d3ace8a07c8b122a579
A <a@example.com>
2025-04-25 23:13:26 +0530

    Cleaned up unnecessary files and folders

--- DysgraphiaDetectionModular/src/path.py
+++
@@ -1,3 +0,0 @@
-import os
-BASE_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "dataset")
-MODEL_SAVE_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "model", "dysgraphia_model.h5")
